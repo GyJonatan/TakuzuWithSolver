9ac03f413a3458aca2d178c56e71fec339b9b6f2
GyJonatan <gyurkovicsjonatan@hotmail.com>
2022-04-26 17:35:06 +0200

    modified the FourByFour page

--- TakuzuWithSolver/Pages/FourByFour.xaml.cs
+++ TakuzuWithSolver/Pages/FourByFour.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TakuzuWithSolver.Logic;
+using System.IO;
 
 namespace TakuzuWithSolver.Pages
 {
@@ -20,14 +22,58 @@ namespace TakuzuWithSolver.Pages
     /// </summary>
     public partial class FourByFour : Page
     {
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            display.Resize(new Size(grid.ActualWidth, grid.ActualHeight));
+            display.InvalidateVisual();
+        }
+
+        private void Grid_Click(object sender, RoutedEventArgs e)
+        {
+            var ClickedPebble = e.OriginalSource as PebbleButton;
+
+            switch (ClickedPebble.pebbleState)
+            {
+                case State.Empty:
+                    ClickedPebble.ImageSource =
+                        new BitmapImage(new Uri(System.IO.Path.Combine("Images", "TileBlue.png"),
+                        UriKind.RelativeOrAbsolute));
+
+                    ClickedPebble.pebbleState = State.Zero;
+                    break;
+
+                case State.Zero:
+                    ClickedPebble.ImageSource =
+                        new BitmapImage(new Uri(System.IO.Path.Combine("Images", "TileGreen.png"),
+                        UriKind.RelativeOrAbsolute));
+
+                    ClickedPebble.pebbleState = State.One;
+                    break;
+
+                case State.One:
+                    ClickedPebble.ImageSource =
+                        new BitmapImage(new Uri(System.IO.Path.Combine("Images", "TileEmpty.png"),
+                        UriKind.RelativeOrAbsolute));
+
+                    ClickedPebble.pebbleState = State.Empty;
+                    break;
+            }
+        }
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+           // controller.KeyPressed(e.Key);
+            display.InvalidateVisual();
+        }
+
         public FourByFour()
         {
+
             InitializeComponent();
+            //LabyLogic logic = new LabyLogic();
+            //display.SetupModel(logic);
+            //controller = new GameController(logic);
         }
 
-        private void playground_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
+        
     }
 }
